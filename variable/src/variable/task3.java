f7d90d842b6795736aa9875e6a8b4e7bf4d8f099
OSANAI-99 <cocosuki99@gmail.com>
2019-12-09 12:15:55 +0900

    変数task3に問題があったため再提出です。

--- variable/src/variable/task3.java
+++ variable/src/variable/task3.java
@@ -4,28 +4,28 @@ public class task3 {
 
 	public static void main(String[] args) {
 
-		int intNum = 219; //メモ:変換前
-		long intNumB = intNum = 219;//メモ:変換後
+		int intBefNum = 219;//メモ:変換前
+		long longAftNum = (long) intBefNum;//メモ:変換後
 		//ここまででひと塊
 
-
-		double doubleNum = 364.2;//メモ:変換前
-		float doubleNumB = (float) doubleNum;//メモ:強制変換後
+		double doubleBefNum = 364.2;//メモ:変換前
+		float doubleAftNum = (float) doubleBefNum;//メモ:強制変換後
 		//ここまででひと塊
 
-		long longNum = 123456L;//メモ:変換前
-		double longNumB = longNum = 123456L;//メモ:変換後
+		long longBefNum = 123456L;//メモ:変換前
+		double longAftNumB = longBefNum;//メモ:変換後（long→double強制は必要ないはず）
+		//ここまででひと塊
 
 		System.out.println("変換前"); //プレビュー1
-		System.out.println("intNum");//メモ:結果
-		System.out.println("doubleNum");//メモ:結果
-		System.out.println("longNum");//メモ:結果
+		System.out.println(intBefNum);//メモ:結果
+		System.out.println(doubleBefNum);//メモ:結果
+		System.out.println(longBefNum);//メモ:結果
 		//ここまで変換前
 
-		System.out.println("変換後"); //プレビュー1
-		System.out.println("intNumBの値");//メモ:結果
-		System.out.println("doubleNumの値: " + doubleNumB);//メモ:結果
-		System.out.println("longNumBの値");//メモ:結果
+		System.out.println("変換後"); //プレビュー2
+		System.out.println(longAftNum);//メモ:結果
+		System.out.println(doubleAftNum);//メモ:結果
+		System.out.println(longAftNumB);//メモ:結果
 		//ここまで変換後
 	}
 
